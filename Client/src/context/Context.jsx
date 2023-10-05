fb2bca450df1f40b496979239f28f6d7c5981d33
judiciousmurich <murichjudicious45@gmail.com>
2023-10-05 22:15:17 +0300

    feat:add styles

--- Client/src/context/Context.jsx
+++ Client/src/context/Context.jsx
@@ -1,26 +1,86 @@
-import { createContext, useEffect, useReducer } from "react";
+import React, { createContext, useEffect, useReducer, useState } from "react";
 import Reducer from './Reducer';
+import axios from "axios";
+import { apiDomain } from "../utils/utilsDomain";
 
-//Initial User
+
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem('user')) || null
-
+  user: JSON.parse(localStorage.getItem('user')) || null,
 }
 
-//Create Context
-export const Context = createContext(INITIAL_STATE);
+export const Context = createContext({
+  ...INITIAL_STATE,
+});
 
-//Provider component
-// eslint-disable-next-line react/prop-types
 export const ContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
-    useEffect(() => {
-        // localStorage.setItem("user", JSON.stringify(state.user))
-    }, [state.user])
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([])
 
-    return (
-        <Context.Provider value={{ user: state.user, dispatch }}>
-            {children}
-        </Context.Provider>
-    )
+
+  const getCartItems = async () => {
+    try {
+      const response = await axios.get(`${apiDomain}/cart`);
+      dispatch({ type: "SET_CART_ITEMS", payload: response.data });
+    } catch (error) {
+      console.error("Error fetching cart items:", error);
+    }
+  };
+
+  useEffect(() => {
+    
+    axios.get(`${apiDomain}/products`)
+      .then((response) => {
+        const imageUrls = [
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1274-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1270-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1268-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1275-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1272-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1273-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1271-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1268-297x223.jpg',
+          'https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/09/Group-1271-297x223.jpg'
+
+        ];
+
+        const productsWithImageURL = response.data.map((product, index) => ({
+          ...product,
+          ImageURL: imageUrls[index], 
+        }));
+
+        setProducts(productsWithImageURL);
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    getCartItems();
+  }, []);
+
+
+  const handleAddToCart = async (product_id) => {
+    try {
+     
+      await axios.post(`${apiDomain}/cart`, { product_id });
+
+      getCartItems();
+
+    
+    } catch (error) {
+      console.error("Error adding item to cart:", error);
+    }
+  };
+
+  
+  useEffect(() => {
+  }, [state.user]);
+
+  return (
+    <Context.Provider value={{ user: state.user, cartItems, dispatch, products, handleAddToCart, setCartItems }}>
+      {children}
+    </Context.Provider>
+  );
 }
